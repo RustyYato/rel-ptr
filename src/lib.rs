1af05b45791a4ceb5713e1857c7b181c1b19d03f
Ozaren <krishna.sd.2012@gmail.com>
2019-03-11 09:43:34 -0700

    added tests for nightly api

--- src/lib.rs
+++ src/lib.rs
@@ -629,4 +629,67 @@ mod tests {
             is_copy::<RelPtr<T, I>>;
         }
     }
+
+    #[cfg(feature = "nightly")]
+    mod nightly {
+        use super::*;
+
+        fn check_trait_object_simple() {
+            use std::fmt::Display;
+
+            let s = unsafe {
+                SelfRef::<[u8; 5], TraitObject<dyn PartialEq<[u8]>>>::new([0, 1, 2, 3, 4], |x| TraitObject::new(x))
+            };
+        
+            assert_eq!(*s.t(), [0, 1, 2, 3, 4]);
+
+            let eq: &[u8] = &[2, 3, 4];
+            assert!(unsafe { s.t_ref().into() } == eq);
+        }
+
+        #[cfg(feature = "no_std")]
+        fn check_trait_object_after_move() {
+            use std::fmt::Display;
+
+            let s = unsafe {
+                SelfRef::<[u8; 5], TraitObject<dyn PartialEq<[u8]>>>::new([0, 1, 2, 3, 4], |x| TraitObject::new(x))
+            };
+        
+            assert_eq!(*s.t(), [0, 1, 2, 3, 4]);
+
+            let eq: &[u8] = &[2, 3, 4];
+            assert!(unsafe { s.t_ref().into() } == eq);
+
+            #[inline(never)]
+            fn force_move<T>(t: T) -> T { t }
+
+            let s = force_move(s);
+        
+            assert_eq!(*s.t(), [0, 1, 2, 3, 4]);
+
+            let eq: &[u8] = &[2, 3, 4];
+            assert!(unsafe { s.t_ref().into() } == eq);
+        }
+
+        #[cfg(not(feature = "no_std"))]
+        fn check_trait_object_after_move() {
+            use std::fmt::Display;
+
+            let s = unsafe {
+                SelfRef::<[u8; 5], TraitObject<dyn PartialEq<[u8]>>>::new([0, 1, 2, 3, 4], |x| TraitObject::new(x))
+            };
+        
+            assert_eq!(*s.t(), [0, 1, 2, 3, 4]);
+
+            let eq: &[u8] = &[2, 3, 4];
+            assert!(unsafe { s.t_ref().into() } == eq);
+
+            let s = Box::new(s);
+        
+            assert_eq!(*s.t(), [0, 1, 2, 3, 4]);
+
+            let eq: &[u8] = &[2, 3, 4];
+            assert!(unsafe { s.t_ref().into() } == eq);
+        }
+    }
 }
