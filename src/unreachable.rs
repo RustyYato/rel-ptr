0d1b98b1742a594ad19bf761237e0ccf12282930
Ozaren <krishna.sd.2012@gmail.com>
2019-05-09 14:08:28 -0700

    updated unchecked_unwrap for better error messages

--- src/unreachable.rs
+++ src/unreachable.rs
@@ -1,4 +1,6 @@
-pub trait UncheckedOptionExt {
+
+/// Adds an unchecked unwrap, this unwrap is UB if self is None
+pub(crate) trait UncheckedOptionExt {
     type T;
 
     unsafe fn unchecked_unwrap(self) -> Self::T;
@@ -7,14 +9,14 @@ pub trait UncheckedOptionExt {
 impl<T> UncheckedOptionExt for Option<T> {
     type T = T;
     
+    #[inline]
     #[allow(clippy::assertions_on_constants)]
     unsafe fn unchecked_unwrap(self) -> T {
         match self {
             Some(value) => value,
-            None => {
-                debug_assert!(false);
-                std::hint::unreachable_unchecked()
-            }
+            None if cfg!(debug_assertions) =>
+                panic!("Tried to use an unset relative pointer, this is UB in release mode!"),
+            None => std::hint::unreachable_unchecked()
         }
     }
 }
